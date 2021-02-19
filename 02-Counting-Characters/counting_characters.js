ab82de9329017ff8c88ec0226289e4e57f6a305d
gando diallo <gandoesprit@gmail.com>
2021-02-19 12:24:14 +0100

    Solution for exercice 2

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,16 @@
 //insert your pseudocode below
 /*
-
-here
+Creare una variabile myString che deve contenere il valore che l'utilizatore ha messo
+Concatenare il valore della lunghezza della variabile myString al suo valore 
+Mostrare nello schermo il valore della variabile e la lugheza della variabile myString 
 
 */
 
 //insert your code below
+
+var myString;
+
+
+myString = window.prompt('Srive una parola');
+myString += String(myString.length);
+window.alert(myString);
